9c50efd0351d1456800c739a2cf40fce87d65901
Adnan Sharif <adnan.sharif.mail@gmail.com>
2018-01-17 14:51:53 +0600

    upto thesis presentation

--- app/src/main/java/info/adnansharif/trafficprediction/MainActivity.java
+++ app/src/main/java/info/adnansharif/trafficprediction/MainActivity.java
@@ -1,13 +1,18 @@
 package info.adnansharif.trafficprediction;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     // UI widget items
@@ -16,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
     private Spinner mValueSource;
     private Spinner mValueDestination;
     private EditText mValue;
+    private HashMap<String, Integer> mPlace;
+    private ArrayList<Double> mLattitude;
+    private ArrayList<Double> mLongitude;
+    private static double expected_time;
 
 
     @Override
@@ -29,11 +38,24 @@ public class MainActivity extends AppCompatActivity {
         mValueSource = findViewById(R.id.value_source_name_spinner);
         mValueDestination = findViewById(R.id.value_destination_name_spinner);
         mValue = findViewById(R.id.value);
+        mPlace = new HashMap<>();
+        mLongitude = new ArrayList<>();
+        mLattitude = new ArrayList<>();
+        expected_time = 0.0;
 
         List<String> items = new ArrayList<>();
         items.add("Please select a place");
         items.add("Mirpur-12");
         items.add("Mirpur-10");
+
+        mPlace.put("Mirpur-12", 0);
+        mPlace.put("Mirpur-10", 1);
+
+        mLattitude.add(23.8280274);
+        mLattitude.add(23.804488);
+
+        mLongitude.add(90.3618099);
+        mLongitude.add(90.3615719);
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item,
                 items);
@@ -43,5 +65,36 @@ public class MainActivity extends AppCompatActivity {
         mDestination.setAdapter(adapter);
         mValueSource.setAdapter(adapter);
         mValueDestination.setAdapter(adapter);
+    }
+
+    public void showMap(View view) {
+        String source = mSource.getSelectedItem().toString();
+        String destination = mDestination.getSelectedItem().toString();
+        if(source.equals(destination)){
+            Toast.makeText(getApplicationContext(), "Source and destination can't be identical", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+        intent.putExtra("lat1", mLattitude.get(mPlace.get(source)));
+        intent.putExtra("long1", mLongitude.get(mPlace.get(source)));
+        intent.putExtra("lat2", mLattitude.get(mPlace.get(destination)));
+        intent.putExtra("long2", mLongitude.get(mPlace.get(destination)));
+        intent.putExtra("time", String.valueOf(expected_time));
+
+        startActivity(intent);
+
+    }
+
+    public void saveData(View view) {
+        Random random = new Random(System.currentTimeMillis());
+        String value = mValue.getText().toString();
+        double v =  Double.valueOf(value);
+        if(expected_time == 0 ) expected_time= v;
+        else{
+            double tmp = Math.abs(expected_time-v);
+            if(v>=expected_time) expected_time+= random.nextDouble()*tmp;
+            else expected_time-= random.nextDouble()*tmp;
+        }
+        Toast.makeText(getApplicationContext(), "Your response is submitted!", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/info/adnansharif/trafficprediction/MapsActivity.java
+++ app/src/main/java/info/adnansharif/trafficprediction/MapsActivity.java
@@ -1,18 +1,38 @@
 package info.adnansharif.trafficprediction;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+
+import org.w3c.dom.Text;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private double lat1, lat2;
+    private double long1, long2;
+    private String time;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +42,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        textView = findViewById(R.id.map_text);
+
+        Bundle extras = getIntent().getExtras();
+        lat1 = extras.getDouble("lat1");
+        long1 = extras.getDouble("long1");
+        lat2 = extras.getDouble("lat2");
+        long2 = extras.getDouble("long2");
+        time = extras.getString("time");
+
+        Log.e("lat", String.valueOf(lat1));
+        Log.e("long", String.valueOf(long1));
+
     }
 
 
@@ -37,10 +69,61 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        Polygon polygon = mMap.addPolygon(new PolygonOptions()
+                .add(new LatLng(lat1, long1), new LatLng(lat2, long2))
+                .strokeColor(Color.RED)
+                .fillColor(Color.BLUE));
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng((lat1+lat2)/2.0, (long1+long2)/2.0)).zoom(14.0f).build();
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+        mMap.moveCamera(cameraUpdate);
+        textView.setText(time+" mins");
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        LatLng sydney = new LatLng(lat1, long1);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng( lat1, long1);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    public Marker addText(final Context context, final GoogleMap map,
+                          final LatLng location, final String text, final int padding,
+                          final int fontSize) {
+        Marker marker = null;
+
+        if (context == null || map == null || location == null || text == null
+                || fontSize <= 0) {
+            return marker;
+        }
+
+        final TextView textView = new TextView(context);
+        textView.setText(text);
+        textView.setTextSize(fontSize);
+
+        final Paint paintText = textView.getPaint();
+
+        final Rect boundsText = new Rect();
+        paintText.getTextBounds(text, 0, textView.length(), boundsText);
+        paintText.setTextAlign(Paint.Align.CENTER);
+
+        final Bitmap.Config conf = Bitmap.Config.ARGB_8888;
+        final Bitmap bmpText = Bitmap.createBitmap(boundsText.width() + 2
+                * padding, boundsText.height() + 2 * padding, conf);
+
+        final Canvas canvasText = new Canvas(bmpText);
+        paintText.setColor(Color.BLACK);
+
+        canvasText.drawText(text, canvasText.getWidth() / 2,
+                canvasText.getHeight() - padding - boundsText.bottom, paintText);
+
+        final MarkerOptions markerOptions = new MarkerOptions()
+                .position(location)
+                .icon(BitmapDescriptorFactory.fromBitmap(bmpText))
+                .anchor(0.5f, 1);
+
+        marker = map.addMarker(markerOptions);
+
+        return marker;
     }
 }
